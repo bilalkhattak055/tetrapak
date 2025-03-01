5117b62d52fbbc50ddf838e28b4708b3f7152c69
bilalkhattak055 <bilalwork055@gmail.com>
2025-03-01 15:56:38 +0500

    electron coming

---
+++ public/electron.js
@@ -0,0 +1,39 @@
+const { app, BrowserWindow } = require('electron');
+const { autoUpdater } = require('electron-updater');
+
+const path = require('path');
+let mainWindow;
+
+app.whenReady().then(() => {
+  autoUpdater.checkForUpdatesAndNotify();
+
+  if (process.platform === 'win32') {
+    app.setAppUserModelId(app.getName());
+  }
+
+  mainWindow = new BrowserWindow({
+    width: 1600,
+    height: 1080, 
+    icon: path.resolve(__dirname, 'icon.ico'),
+    fullscreenable: true, 
+    autoHideMenuBar: false, 
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+
+  mainWindow.maximize(); 
+
+  mainWindow.loadURL('https://tetrapak.vercel.app'); 
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
